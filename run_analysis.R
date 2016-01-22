a3ca506a9363de24f12bd0e6b0cbb83bc3c3488e
ckild <ireneday24@gmail.com>
2016-01-22 10:32:34 -0800

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -28,22 +28,22 @@ xtest<-read.table("./test/X_test.txt",header=FALSE)
 ytrain<-read.table("./train/y_train.txt",header=FALSE,col.names = "activity")
 ytest<-read.table("./test/y_test.txt",header=FALSE,col.names = "activity")
 
-# 4. Appropriately labels the data set with descriptive variable names.
+# Appropriately labels the data set with descriptive variable names.
 names(xtest)<-features[,2]
 names(xtrain)<-features[,2]
 
-#Consolidate training data & test data
+# Consolidate training data & test data
 training<-cbind(subjecttrain,ytrain,xtrain)
 testing<-cbind(subjecttest,ytest,xtest)
-# 1. Merges the training and the test sets to create one data set.
+# Merges the training and the test sets to create one data set.
 data<-rbind(training,testing)
-# 3. Uses descriptive activity names to name the activities in the data set
+# Uses descriptive activity names to name the activities in the data set
 data$activity<-factor(data$activity, levels = activitylabels[,1], labels = activitylabels[,2])
 
-# 2. Extracts only the measurements on the mean and standard deviation 
+# Extracts only the measurements on the mean and standard deviation 
 #    for each measurement.
 neededcol<-grepl("mean()|std()|subject|activity",names(data)) & !grepl("meanFreq", names(data))
 finaldata<-data[,neededcol]
-# 5. From the data set in step 4, creates a second, independent tidy data set 
-#    with the average of each variable for each activity and each subject.
+# From the data set in step 4, creates a second, independent tidy data set 
+# with the average of each variable for each activity and each subject.
 write.table(finaldata,file="tidy_data.txt",row.names=FALSE)
