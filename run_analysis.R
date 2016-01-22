68eea5060f6e5e7005641557dec7790e05ea8d23
ckild <ireneday24@gmail.com>
2016-01-22 10:31:06 -0800

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,6 +1,9 @@
 # Getting and Cleaning Data Project
-filename<-"project.zip"
+library(dplyr)
+# Set the work directory to project
 setwd("~/R/project")
+filename<-"project.zip"
+
 # Download the file and unzip it if it is not exist
 if(!file.exists(filename))
   {
@@ -11,16 +14,19 @@ if(!file.exists("UCI HAR Dataset"))
   {
     unzip(filename)
 }
+
+# Change work directory to UCI HAR Dataset
 setwd("~/R/project/UCI HAR Dataset")
+
 # Import data
 features<-read.table("features.txt")
 activitylabels<-read.table("activity_labels.txt")
-subjecttrain<-read.table("./train/subject_train.txt",header=FALSE)
-subjecttest<-read.table("./test/subject_test.txt",header=FALSE)
+subjecttrain<-read.table("./train/subject_train.txt",header=FALSE,col.names ="subject")
+subjecttest<-read.table("./test/subject_test.txt",header=FALSE,col.names ="subject")
 xtrain<-read.table("./train/X_train.txt",header=FALSE)
 xtest<-read.table("./test/X_test.txt",header=FALSE)
-ytrain<-read.table("./train/y_train.txt",header=FALSE)
-ytest<-read.table("./test/y_test.txt",header=FALSE)
+ytrain<-read.table("./train/y_train.txt",header=FALSE,col.names = "activity")
+ytest<-read.table("./test/y_test.txt",header=FALSE,col.names = "activity")
 
 # 4. Appropriately labels the data set with descriptive variable names.
 names(xtest)<-features[,2]
@@ -31,14 +37,13 @@ training<-cbind(subjecttrain,ytrain,xtrain)
 testing<-cbind(subjecttest,ytest,xtest)
 # 1. Merges the training and the test sets to create one data set.
 data<-rbind(training,testing)
-# rename first 2 column names since they are duplicate
-names(data)[1:2]<-c("subject","activity")
 # 3. Uses descriptive activity names to name the activities in the data set
 data$activity<-factor(data$activity, levels = activitylabels[,1], labels = activitylabels[,2])
 
 # 2. Extracts only the measurements on the mean and standard deviation 
 #    for each measurement.
-neededcol<-grep("mean()|std()",names(data))
+neededcol<-grepl("mean()|std()|subject|activity",names(data)) & !grepl("meanFreq", names(data))
+finaldata<-data[,neededcol]
 # 5. From the data set in step 4, creates a second, independent tidy data set 
 #    with the average of each variable for each activity and each subject.
-write.table(,row.names=FALSE)
+write.table(finaldata,file="tidy_data.txt",row.names=FALSE)
